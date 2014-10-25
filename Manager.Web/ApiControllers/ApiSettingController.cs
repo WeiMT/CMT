5586b8ba29c3d95fc10c0b5889e7f2c2022d4e7e
yinshaobin <jjwind320@sohu.com>
2014-10-25 20:41:51 +0800

    管理端页面上传

---
+++ Manager.Web/ApiControllers/ApiSettingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess.Models;
+
+namespace Manager.Web.ApiControllers
+{
+    public class ApiSettingController : ApiController
+    {
+        [HttpGet]
+        public dynamic GetCarTypes()
+        {
+            using (var ctx = new CarHealthEntities())
+            {
+                var cartTypes = ctx.CarTypes.Take(100).ToList();
+                return cartTypes;
+            }
+        }
+    }
+}
