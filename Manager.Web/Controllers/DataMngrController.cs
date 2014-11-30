1bfdea99f6d96f2ba5bd4bae0f3e118547bd5e53
yinshaobin <jjwind320@sohu.com>
2014-12-01 02:29:22 +0800

    新版kendoui及bootstrap

---
+++ Manager.Web/Controllers/DataMngrController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manager.Web.Controllers
+{
+    public class DataMngrController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("CarType");
+        }
+
+        public ActionResult CarType()
+        {
+            return View();
+        }
+    }
+}
