5fd2a88473c757fe485c38f2f64246921ff92d6a
yinshaobin <jjwind320@sohu.com>
2014-12-22 10:51:56 +0800

    距离计算

--- ToolConsole/Program.cs
+++ ToolConsole/Program.cs
@@ -44,7 +44,32 @@ namespace ToolConsole
 
             //GetAdminDivisions();
 
-            ProcessAdminDivisions();
+            //ProcessAdminDivisions();
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            for (int n = 0; n < 50; n++)
+            {
+                sw.Reset();
+                sw.Start();
+
+                var dis = GetDistance(115.883408, 28.683199, 115.888338, 28.676728);
+
+                sw.Stop();
+
+                Console.WriteLine(dis + "耗时：" + sw.ElapsedMilliseconds + "||" + sw.ElapsedTicks);
+
+                sw.Reset();
+                sw.Start();
+
+                dis = GetDistance(115.883408, 28.683199, 115.892903, 28.68775);
+
+                sw.Stop();
+
+                Console.WriteLine(dis + "耗时：" + sw.ElapsedMilliseconds + "||" + sw.ElapsedTicks);
+            }                       
+
+            Console.ReadKey();
+
         }
 
         private static void AliyunOssCors()
@@ -269,6 +294,26 @@ namespace ToolConsole
             }
 
             Console.ReadKey();
+        }
+
+        private const double EARTH_RADIUS = 6378.137;//地球半径
+        private static double rad(double d)
+        {
+            return d * Math.PI / 180.0;
+        }
+
+        public static double GetDistance(double lng1, double lat1, double lng2, double lat2)
+        {
+            double radLat1 = rad(lat1);
+            double radLat2 = rad(lat2);
+            double a = radLat1 - radLat2;
+            double b = rad(lng1) - rad(lng2);
+
+            double s = 2 * Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(a / 2), 2) +
+             Math.Cos(radLat1) * Math.Cos(radLat2) * Math.Pow(Math.Sin(b / 2), 2)));
+            s = s * EARTH_RADIUS;
+            s = Math.Round(s * 10000) / 10000;
+            return s;
         }
     }
 }
---
+++ User.Service/Helpers/LngLatDistanceHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace User.Service.Helpers
+{
+    public static class LngLatDistanceHelper
+    {
+        private const double EarthRadius = 6378.137;//地球半径
+
+        private static double Rad(double d)
+        {
+            return d * Math.PI / 180.0;
+        }
+
+        public static double GetDistance(double lng1, double lat1, double lng2, double lat2)
+        {
+            double radLat1 = Rad(lat1);
+            double radLat2 = Rad(lat2);
+            double a = radLat1 - radLat2;
+            double b = Rad(lng1) - Rad(lng2);
+
+            double s = 2 * Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(a / 2), 2) +
+             Math.Cos(radLat1) * Math.Cos(radLat2) * Math.Pow(Math.Sin(b / 2), 2)));
+            s = s * EarthRadius;
+            s = Math.Round(s * 10000) / 10000;
+            return s;
+        } 
+    }
+}
